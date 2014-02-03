3884bcdee05f42c01a2d5a962ec6f0acf853261e
Chad Dotson <chad.dotson@gmail.com>
2014-02-02 22:58:37 -0600

    Got a scratch working...

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
@@ -12,4 +13,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^scratch.html', TemplateView.as_view(template_name='mysite/scratch.html'), name="home"),
 )
